77f96b3e2d8c4f9b54217431b93701ce97b7fa95
Alex Bevan <AlexBevan@users.noreply.github.com>
2020-07-15 11:27:44 +0100

    Update Add-TerraformServicePrinciple.ps1

--- Add-TerraformServicePrinciple.ps1
+++ Add-TerraformServicePrinciple.ps1
@@ -35,20 +35,52 @@ function Add-TerraformServicePrinciple {
 
         $resourceID = (az ad sp show --id 00000003-0000-0000-c000-000000000000 | convertfrom-json).objectId
         
-        $body = @{
-            clientId    = $sp_ob_id
-            consentType = "AllPrincipals"
-            principalId = $null
-            resourceId  = $resourceID
-            scope       = "User.Read User.Read.All User.ReadBasic.All User.ReadWrite User.ReadWrite.All Group.Read.All Group.ReadWrite.All Domain.Read.All Directory.Read.All GroupMember.Read.All GroupMember.ReadWrite.All"
-            startTime   = "2019-10-19T10:37:00Z"
-            expiryTime  = "2019-10-19T10:37:00Z"
-        }
-        $apiUrl = "https://graph.microsoft.com/beta/oauth2PermissionGrants"
-        Try {
-            $r = Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization = "Bearer $($token)" }  -Method POST -Body $($body | convertto-json) -ContentType "application/json"
-           }
-        catch {Failure}
+        # $body = @{
+        #     clientId    = $sp_ob_id
+        #     consentType = "AllPrincipals"
+        #     principalId = $null
+        #     resourceId  = $resourceID
+        #     scope       = "Application.ReadWrite.All Application.ReadWrite.OwnedBy Directory.ReadWrite.All Group.ReadWrite.All"
+        #     startTime   = "2019-10-19T10:37:00Z"
+        #     expiryTime  = "2019-10-19T10:37:00Z"
+        # }
+        # $apiUrl = "https://graph.microsoft.com/beta/oauth2PermissionGrants"
+        # Try {
+        #     $r = Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization = "Bearer $($token)" }  -Method POST -Body $($body | convertto-json) -ContentType "application/json"
+        #    }
+        # catch {Failure}
+
+        $json = @'
+[
+    {
+        "resourceAppId": "00000002-0000-0000-c000-000000000000",
+        "resourceAccess": [
+            {
+                "id": "970d6fa6-214a-4a9b-8513-08fad511e2fd",
+                "type": "Scope"
+            },
+            {
+                "id": "1cda74f2-2616-4834-b122-5cb1b07f8a59",
+                "type": "Role"
+            },
+            {
+                "id": "824c81eb-e3f8-4ee6-8f6d-de7f50d565b7",
+                "type": "Role"
+            },
+            {
+                "id": "78c8a3c8-a07e-4b9e-af1b-b5ccab50a175",
+                "type": "Role"
+            }
+        ]
+    }
+]
+'@
+        out-file test.json
+        Add-Content -Path testjson.json -Value $json
+        $api = (az ad app update --id $sp.appId --required-resource-accesses testjson.json)
+        remove-item ./testjson.json -force  
+        az ad app permission admin-consent --id $sp.appId
+
     }
     END
     {
@@ -58,5 +90,4 @@ function Add-TerraformServicePrinciple {
         
         
     }
-
 
